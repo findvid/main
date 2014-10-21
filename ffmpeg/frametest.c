103b886f96001749665da9c84f51d04aab56e4cb
Anonymer Eintrag <jbusc002@lx1-06.local.cs.hs-rm.de>
2014-10-21 09:17:35 +0200

    Moved framecounter check to loop head

--- ffmpeg/frametest.c
+++ ffmpeg/frametest.c
@@ -15,9 +15,9 @@ void SaveFrame(AVFrame *pFrame, int width, int height, int i) {
 		return;
    
 
-	//Print header for .ppm-format
+	// Print header for .ppm-format
 	fprintf(pFile, "P6\n%d %d\n255\n", width, height);   
-	//write bytevectors into file
+	// Write bytevectors into file
 	for(y = 0; y < height; y++)
 		fwrite(pFrame->data[0]+y*pFrame->linesize[0], 1, width*3, pFile);
 								  
@@ -26,10 +26,10 @@ void SaveFrame(AVFrame *pFrame, int width, int height, int i) {
 }
 
 int main(int argc, char** argv) {
-	//Registers all available codecs
+	// Registers all available codecs
 	av_register_all();
 
-	//Struct holding information about container-file
+	// Struct holding information about container-file
 	AVFormatContext * pFormatCtx = NULL;
 
 	if (argc < 2) {
@@ -37,7 +37,7 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 
-	//Read format information into struct
+	// Read format information into struct
 	if(avformat_open_input(&pFormatCtx, argv[1], NULL, NULL) != 0)
 		return -1;
 	
@@ -45,7 +45,7 @@ int main(int argc, char** argv) {
 	if(avformat_find_stream_info(pFormatCtx, NULL)<0)
 		return -1; // Couldn't find stream information
 	
-	//Search for a video stream, assuming there's only one, we pick the first and continue
+	// Search for a video stream, assuming there's only one, we pick the first and continue
 	int videoStream = -1;
 	for (int i = 0; i < pFormatCtx->nb_streams; i++ )
 		if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
@@ -53,11 +53,11 @@ int main(int argc, char** argv) {
 			break;
 		}
 	
-	//Could not find a video stream
+	// Could not find a video stream
 	if (videoStream == -1)
 		return -1;
 
-	//retrieve CodecContext
+	// Retrieve CodecContext
 	AVCodecContext *pCodecCtx = pFormatCtx->streams[videoStream]->codec;
 	
 	// Get the appropiate Decoder for the video stream
@@ -73,7 +73,7 @@ int main(int argc, char** argv) {
 
 	// Allocate frame to read packets into
 	AVFrame *pFrame = av_frame_alloc();
-	// Aoolcate frame to read conversion into
+	// Allocate frame to read conversion into
 	AVFrame *pFrameRGB = av_frame_alloc();
 
 	if (pFrame == NULL || pFrameRGB == NULL)
@@ -83,35 +83,34 @@ int main(int argc, char** argv) {
 	int numBytes = avpicture_get_size(PIX_FMT_RGB24, pCodecCtx->width, pCodecCtx->height);
 	uint8_t *buffer = (uint8_t *)av_malloc(numBytes * sizeof(uint8_t));
 
-	//Initialize pFrameRGB as empty frame
+	// Initialize pFrameRGB as empty frame
 	avpicture_fill((AVPicture *)pFrameRGB, buffer, PIX_FMT_RGB24, pCodecCtx->width, pCodecCtx->height);
 
-	//Object needed to perform conversions from a source dimension to a destination dimension using certain filters
+	// Object needed to perform conversions from a source dimension to a destination dimension using certain filters
 	struct SwsContext *img_convert_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height, PIX_FMT_RGB24, SWS_BICUBIC, NULL, NULL, NULL);
 	
-	//Finally, start reading packets from the file
+	// Finally, start reading packets from the file
 	int i = 0;
 	int frameCount = 0;
 	int frameFinished = 0;
 	AVPacket packet;
-	//Mind that we read from pFormatCtx, which is the general container file...
-	while (av_read_frame(pFormatCtx, &packet)>=0) {
+	// Mind that we read from pFormatCtx, which is the general container file...
+	while (av_read_frame(pFormatCtx, &packet) >= 0 && frameCount <= 5) {
 		// ... therefore, not every packet belongs to our video stream!
-		if (packet.stream_index==videoStream) {
-			// packet is part of the video stream previously found
+		if (packet.stream_index == videoStream) {
+			// Packet is part of the video stream previously found
 			// therefore use the CodecContext previously retrieved with
 			// avcodec_find_decoder2 to read the packet into pFrame
 			int len = avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, &packet);
 			if (len<0) return -1;
-			//frameFinished is set be avcodec_decode_video2 accordingly
+			// frameFinished is set be avcodec_decode_video2 accordingly
 			if (frameFinished) {
 				frameCount++;
-				//Convert pFrame into a simple bitmap format
+				// Convert pFrame into a simple bitmap format
 				sws_scale(img_convert_ctx,(const uint8_t* const*)pFrame->data, pFrame->linesize, 0, pCodecCtx->height, pFrameRGB->data, pFrameRGB->linesize);
 				
 				// and save the first 5 frames to disk. Because we can
-				if (frameCount<=5)
-					SaveFrame(pFrameRGB, pCodecCtx->width, pCodecCtx->height, frameCount);
+				SaveFrame(pFrameRGB, pCodecCtx->width, pCodecCtx->height, frameCount);
 
 			}
 
