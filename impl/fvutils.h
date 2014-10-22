6107846375a987aaac8f100df7612ea60d05fd8e
Dennis Müller <dennis.mueller@student.hs-rm.de>
2014-10-22 16:24:22 +0200

    Basis-Branch + setPixel-Makro

---
+++ impl/fvutils.h
@@ -0,0 +1,8 @@
+//Stimmt so noch nicht, muss mehrzeiliges Makro definieren, um alle Farbkomponente zu definieren
+#define setPixel(p,x,y,c) \
+	do { \
+	p->data[0][x * 3 + y*p->linesize[0]] = ((uint8_t)(c>>16)); \
+	p->data[0][x * 3 + y*p->linesize[0] + 1] = ((uint8_t)(c>>8)); \
+	p->data[0][x * 3 + y*p->linesize[0] + 2] = (uint8_t)c; \
+	} while(0)
+
