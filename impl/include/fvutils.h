b3db1140821a29e352a1875ff0c55a4e8fe8000f
Dennis Müller <dennis.mueller@student.hs-rm.de>
2014-10-23 13:30:36 +0200

    helper function for copying frames

--- impl/include/fvutils.h
+++ impl/include/fvutils.h
@@ -1,3 +1,4 @@
+#include <libswscale/swscale.h>
 #define setPixel(p,x,y,c) \
 	do { \
 	p->data[0][x * 3 + y*p->linesize[0]] = ((uint8_t)(c>>16)); \
@@ -5,3 +6,16 @@
 	p->data[0][x * 3 + y*p->linesize[0] + 2] = (uint8_t)c; \
 	} while(0)
 
+#define getPixel(p,x,y) ((p->data[0][x * 3 + y*p->linesize[0]]<<16) + (p->data[0][x * 3 + y*p->linesize[0] + 1]<<8) + p->data[0][x * 3 + y*p->linesize[0] + 2])
+
+
+AVFrame * copyFrame(AVFrame *pPic, struct SwsContext * ctx, int width, int height) {
+	AVFrame *res = av_frame_alloc();
+	avpicture_alloc((AVPicture *)res, PIX_FMT_RGB24, width, height);
+	int numBytes = avpicture_get_size(PIX_FMT_RGB24, width, height);
+	uint8_t *buffer = (uint8_t *)av_malloc(numBytes * sizeof(uint8_t));
+
+	avpicture_fill((AVPicture *)res, buffer, PIX_FMT_RGB24, width, height);
+	sws_scale(ctx, (const uint8_t * const*)pPic->data, pPic->linesize, 0, height, res->data, res->linesize);
+	return res;
+}
