f71e44463f0dc899d695b1e0ff5217acae91f1f6
Johannes <adnap9449@gmail.com>
2014-10-30 00:22:06 +0100

    Added header for histograms

---
+++ impl/include/histograms.h
@@ -0,0 +1,49 @@
+#pragma once
+
+
+/**
+ *
+ */
+#define GETBINHSV(hsv) ((hsv.h / 45) << 4) + ((hsv.s >> 6) << 2) + (hsv.v >> 6)
+
+/**
+ * 
+ */
+#define GETBINRGB(img, p) ((img[p] >> 6) << 4) + ((img[p+1] >> 6) << 2) + (img[p+2] >> 6)
+
+/**
+ * Allocates a 4x4x4 color histogram
+ *
+ * @return	pointer to the newly allocated histogram
+ */
+uint32_t* newHistRgb();
+
+/**
+ * Allocates a 8x4x4 color histogram
+ *
+ * @return	pointer to the newly allocated histogram
+ */
+uint32_t* newHistHsv();
+
+/**
+ * Fills a color histogram with data from a frame
+ * The histogram is a 4x4x4 RGB-histogram
+ *
+ * @hist	pointer to the histogram array
+ * @img		the image a histogram should be created for
+ * @h		height of the frame
+ * @w		width of the frame
+ */
+void fillHistRgb(uint32_t* hist, AVFrame* img, int h, int w);
+
+/**
+ * Fills a color histogram with data from a frame
+ * The histogram is a 8x4x4 HSV-histogram
+ *
+ * @hist	pointer to the histogram array
+ * @img		the image a histogram should be created for
+ * @h		height of the frame
+ * @w		width of the frame
+ */
+void fillHistHsv(uint32_t* hist, AVFrame* img, int h, int w);
+
