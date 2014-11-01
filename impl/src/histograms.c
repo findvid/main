579a2638aaa221df22c074672f9f2e105847c329
Johannes <adnap9449@gmail.com>
2014-11-01 14:45:04 +0100

    Commented a bit

--- impl/src/histograms.c
+++ impl/src/histograms.c
@@ -5,28 +5,42 @@
 #include "histograms.h"
 
 typedef struct {
-	int h;
-	int s;
-	int v;
+	int h;	// 0..360
+	int s;	// 0..255
+	int v;	// 0..255
 } HSV;
 
 #define MAX(a, b) ((a>b) ? a : b)
 #define MIN(a, b) ((a<b) ? a : b)
 
+/**
+ * Converts rgb colors to hsv
+ *
+ * @param hsv	pointer to the hsv struct
+ * @param r	red 0..255
+ * @param g	green 0..255
+ * @param b	blue 0..255
+ */
 void rgbToHsv(HSV* hsv, int r, int g, int b) {
 	int min, max;
+	// Get the minimum of the colors
 	min = MIN(r,g);
 	min = MIN(min,b);
+	// Get the maximum of the colors
 	max = MAX(r,g);
 	max = MAX(max,b);
 
+	// Set the value
+	hsv->v = max;
+
+	// Catch gray tones
 	if (min == max) {
 		hsv->h = 0;
 		hsv->s = 0;
-		hsv->v = min;
 		return;
 	}
 
+	// Calculate the hue
 	if (max == r) {
 		hsv->h = 60 * ((g - b) / (max - min));
 	} else if (max == g) {
@@ -39,9 +53,9 @@ void rgbToHsv(HSV* hsv, int r, int g, int b) {
 		hsv->h += 360;
 	}
 
+	// Calculate the saturation
 	hsv->s = (255 * (max - min)) / max;
 
-	hsv->v = max;
 }
 
 
