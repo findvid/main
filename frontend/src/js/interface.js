c6d6ac0be3dd472c8a2c3fd1f71d0f7782b70e51
Patrick Henrici <patrick.henrici@student.hs-rm.de>
2014-11-02 13:30:49 +0000

    neues design umgesetzt

--- frontend/src/js/interface.js
+++ frontend/src/js/interface.js
@@ -2,4 +2,12 @@ $(function() {
 	$('.menu .searchnavi .icon.closeicon').on('click', function() {
 		$('.menu .searchnavi .searchfield').val('');
 	});
+
+	$('.uploadwindow').on('mouseenter', function() {
+		$('.uploadwrap').addClass('active');
+	});
+
+	$('.uploadwindow').on('mouseleave', function() {
+		$('.uploadwrap').removeClass('active');
+	});
 });
