535bb24f52d2508607dfd5cb92a8161df44397f0
Patrick Henrici <patrick@opeey.de>
2014-11-08 19:44:19 +0100

    new version after 7-hour hacking :)

--- frontend/src/cherry.py
+++ frontend/src/cherry.py
@@ -1,25 +1,17 @@
 import cherrypy
+import pymongo
 import os
 import re
 
-HTMLDIR = os.path.join(os.path.abspath('.'), u"html")
+MONGOCLIENT = pymongo.MongoClient()
+DB = MONGOCLIENT['findvid']
+VIDEOS = DB['videos']
+
+HTMLDIR = os.path.join(os.path.abspath('.'), "html")
 DEBUG = True
 
-def renderTemplate(config):
-	tplfile = open(os.path.join(HTMLDIR, u"template.html")).read()
-
-	# Check if there is content specified in config
-	try:
-		config['content']
-	except NameError:
-		config['content'] = None
-
-	# If there is no content in config, raise ServerError in debug or just return unrendered template
-	if config['content'] is None:
-		if DEBUG:
-			raise cherrypy.HTTPError(500, "The Server couldn't create the page due to a misconfiguration.")
-		else:
-			return tplfile
+def renderTemplate(filename, config):
+	tplfile = open(os.path.join(HTMLDIR, filename)).read()
 
 	# Replace each placeholder with the information in config
 	for key, value in config.items():
@@ -30,18 +22,74 @@ def renderTemplate(config):
 class Root(object):
 	@cherrypy.expose
 	def index(self):
+		videosFromDb = VIDEOS.find()
+
+		videos = []
+
+		for video in videosFromDb:
+			fps = int(video['fps'])
+			filename = str(video['filename'])
+
+			lengthInSec = int(int(video['framecount'])/fps)
+			lengthSec = lengthInSec % 60
+			lengthMin = int(lengthInSec / 60) % 60
+			lengthHr = int(lengthInSec / 60 / 60) % 60
+
+			videoconfig = {
+				'thumbnail': 'images/thumb.png',
+				'filename': filename,
+				'length': "%1.2d" % lengthHr + ":" + "%1.2d" % lengthMin + ":" + "%1.2d" % lengthSec
+			}
+
+			scenes = []
+
+			for scene in video['scenes']:
+
+				startInSec = int(int(scene['startframe'])/fps)
+				startSec = startInSec % 60
+				startMin = int(startInSec / 60) % 60
+				startHr = int(startInSec / 60 / 60) % 60
+ 
+				endInSec = int(int(scene['endframe'])/fps)
+				endSec = endInSec % 60
+				endMin = int(endInSec / 60) % 60
+				endHr = int(endInSec / 60 / 60) % 60
+
+				sceneconfig = {
+					'thumbnail': 'images/thumb.png',
+					'filename': filename,
+					'scenecount': str(int(scene['_id'])),
+					'starttime': "%1.2d" % startHr + ":" + "%1.2d" % startMin + ":" + "%1.2d" % startSec,
+					'endtime': "%1.2d" % endHr + ":" + "%1.2d" % endMin + ":" + "%1.2d" % endSec
+				}
+
+				scenes.append(renderTemplate('scene.html', sceneconfig))
+
+			videos.append({'html': renderTemplate('video.html', videoconfig), 'scenes': scenes})
+
+		content = ""
+		for video in videos:
+			for scene in video['scenes']:
+				content += scene
+
 		config = {
 			'title': 'find.vid - The Videosearch Engine',
 			'videocount': '0',
 			'scenecount': '0',
-			'content': 'empty'
+			'content': content
 		}
-		return renderTemplate(config)
+		return renderTemplate('template.html', config)
+
+class Test(object):
+	@cherrypy.expose
+	def index(self):
+		return "test"
 
 if __name__ == '__main__':
 	cherrypy.config.update('./global.conf')
 	
 	cherrypy.tree.mount(Root(), "/", './app.conf')
+	cherrypy.tree.mount(Test(), "/test", './app.conf')
 
 	if hasattr(cherrypy.engine, 'block'):
 		# 3.1 syntax
