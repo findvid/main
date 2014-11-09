77a90e09496a09106801634101f46cd96f8b535e
Johannes <adnap9449@gmail.com>
2014-11-09 02:23:15 +0100

    Split of main almost done

--- impl/src/shotbounds.c
+++ impl/src/shotbounds.c
@@ -236,65 +236,61 @@ int main(int argc, char **argv) {
 	return (int)cuts; //Must return a struct for to contain array boundary aswell...
 }
 
-int findCuts(AVFormatContext *pFormatCt, AVCodecContext *pCodecCtx, int video_stream, uint32_t *cuts);	
+uint32_t mergeListsToArray(LargeList *l1, LargeList *l2, uint32_t *array) {
+	// Get the maximum size the array will have
+	uint32_t size = l1->size + l2->size;
+	// Allocate the array
+	array = calloc(sizeof(uint32_t), size);
 
-int processVideo(char *filename, uint32_t *cuts) {
-	// Registers all available codecs
-	av_register_all(); // TODO Move this somewhere else
-
-	// Struct holding information about container-file
-	AVFormatContext *pFormatCtx = NULL;
-
-	// Read format information into struct
-	if(avformat_open_input(&pFormatCtx, filename, NULL, NULL) != 0) {
-		// TODO Errorhandleing
-		return -1;
-	}
-	
-	// Retrieve stream information
-	if(avformat_find_stream_info(pFormatCtx, NULL)<0) {
-		// TODO Errorhandleing
-		return -1; // Couldn't find stream information
-	}
-	
-	// Search for a video stream, assuming there's only one, we pick the first and continue
-	int videoStream = -1;
-	for (int i = 0; i < pFormatCtx->nb_streams; i++ ) {
-		if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
-			videoStream = i;
-			break;
+	uint32_t i = 0;
+	void *v_e;
+	void *v_c;
+	// Get the iterators
+	ListIterator *iter_e = list_iterate(l1);
+	ListIterator *iter_c = list_iterate(l2);
+	v_e = list_next(iter_e);
+	v_c = list_next(iter_c);
+	// while both lists contain something...
+	while ((v_e != NULL) && (v_c != NULL)) {
+		uint32_t e = (uint32_t)v_e;
+		uint32_t c = (uint32_t)v_c;
+		// ...merge their values.
+		if ( e == c ) {
+			array[i++] = c;
+			v_e = list_next(iter_e);
+			v_c = list_next(iter_c);
+		} else if (e < c) {
+			array[i++] = e;
+			v_e = list_next(iter_e);
+		} else {
+			array[i++] = c;
+			v_c = list_next(iter_c);
 		}
 	}
-	
-	// Could not find a video stream
-	if (videoStream == -1) {
-		// TODO Errorhandleing
-		return -1;
-
-	// Retrieve CodecContext
-	AVCodecContext *pCodecCtx = pFormatCtx->streams[videoStream]->codec;
-	
-	// Get the appropiate Decoder for the video stream
-	AVCodec *pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
-	if (pCodec == NULL) {
-		// TODO Errorhandleing
-		printf("Unsupported Codec\n");
-		return -1;
+	// Write the left over list into the array too
+	while(v_e != NULL) {
+		array[i++] = (uint32_t)v_e;
+		v_e = list_next(iter_e);
 	}
-	
-	if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0) {
-		// TODO Errorhandleing
-		return -1;
+	while(v_c != NULL) {
+		array[i++] = (uint32_t)v_c;
+		v_c = list_next(iter_c);
 	}
+	free(iter_e);
+	free(iter_c);
+	// Return how many different values were actually written into the array
+	return i;
+}
 
+int findCuts(AVFormatContext *pFormatCtx, AVCodecContext *pCodecCtx, int videoStream, uint32_t *cuts) {
 	// Allocate frame to read packets into
 	AVFrame *pFrame = av_frame_alloc();
-	if (!pframe) {
+	if (!pFrame) {
 		// TODO Errorhandleing
 		return -1;
 	}
 	AVFrame *pFrameRGB24 = av_frame_alloc();
-	if (!pFrameRGB24 == NULL) {
+	if (!pFrameRGB24) {
 		// TODO Errorhandleing / frees
 		return -1;
 	}
@@ -303,12 +299,6 @@ int processVideo(char *filename, uint32_t *cuts) {
 		return -1;
 	}
 	
-	/* TODO Remove this. Has been replaced with the call below
-	int numBytes = avpicture_get_size(PIX_FMT_RGB24, DESTINATION_WIDTH, DESTINATION_HEIGHT);
-	uint8_t *buffer_rgb24 = (uint8_t *)av_malloc(numBytes * sizeof(uint8_t));
-	avpicture_fill((AVPicture *)pFrameRGB24, buffer_rgb24, PIX_FMT_RGB24, DESTINATION_WIDTH, DESTINATION_HEIGHT);
-	*/
-
 	// Object needed to perform conversions from a source dimension to a destination dimension using certain filters
 	// Convert into a smaller frame for easier processing
 	struct SwsContext *convert_rgb24 = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt, DESTINATION_WIDTH, DESTINATION_HEIGHT, PIX_FMT_RGB24, SWS_BICUBIC, NULL, NULL, NULL);
@@ -334,9 +324,9 @@ int processVideo(char *filename, uint32_t *cuts) {
 	feedback_edges.diff = NULL;
 
 	// TODO Replace with ColorHistFeedback
-	ShotFeedback feedback_colors;
-	feedback_colors.lastFrame = NULL;
-	feedback_colors.diff = NULL;
+	//ShotFeedback feedback_colors;
+	//feedback_colors.lastFrame = NULL;
+	//feedback_colors.diff = NULL;
 	//should be copied into the new internal var-array and then be freed by the feature detection
 
 	// Mind that we read from pFormatCtx, which is the general container file...
@@ -360,15 +350,9 @@ int processVideo(char *filename, uint32_t *cuts) {
 				
 				list_push(list_frames, pFrameRGB24);
 
-				/* TODO Remove this comment
 				//!!!!! Allocate a new frame, obviously
 				pFrameRGB24 = av_frame_alloc();
-				numBytes = avpicture_get_size(PIX_FMT_RGB24, DESTINATION_WIDTH, DESTINATION_HEIGHT);
-				buffer_rgb24 = (uint8_t *)av_malloc(numBytes * sizeof(uint8_t));
-				avpicture_fill((AVPicture *)pFrameRGB24, buffer_rgb24, PIX_FMT_RGB24, DESTINATION_WIDTH, DESTINATION_HEIGHT);
-				*/
-				pFrameRGB24 = av_frame_alloc();
-				if (!pFrameRGB24 == NULL) {
+				if (!pFrameRGB24) {
 					// TODO Errorhandleing / frees
 					return -1;
 				}
@@ -405,7 +389,7 @@ int processVideo(char *filename, uint32_t *cuts) {
 	//Process the (remaining) frames
 					
 	//call a method to fill list_cuts with detected cut frames
-	detectCutsByEdges(list_frames, list_cuts_edges, frameBulk, &feedback_edges, convert_g8, DESTINATION_WIDTH, DESTINATION_HEIGHT);
+	detectCutsByEdges(list_frames, list_cuts_edges, bulkStart, &feedback_edges, convert_g8, DESTINATION_WIDTH, DESTINATION_HEIGHT);
 	
 	//Final feedback goes nowhere
 	free(feedback_edges.diff);
@@ -413,45 +397,8 @@ int processVideo(char *filename, uint32_t *cuts) {
 
 	list_forall(list_frames, av_free);
 	list_destroy(list_frames);
-	
-	//Finally, sort the cuts and return it as one coherent array
-	uint32_t c_cuts = (list_cuts_edges->size + list_cuts_colors->size);
-	cuts = calloc(sizeof(uint32_t), c_cuts);
 
-	// TODO Move merging to own function
-	int i = 0;
-	void * v_e;
-	void * v_c;
-	ListIterator * iter_e = list_iterate(list_cuts_edges);
-	ListIterator * iter_c = list_iterate(list_cuts_colors);
-	v_e = list_next(iter_e);
-	v_c = list_next(iter_c);
-	while ((v_e != NULL) && (v_c != NULL)) {	
-		uint32_t e = (uint32_t)v_e;
-		uint32_t c = (uint32_t)v_c;
-		if ( e == c ) {
-			cuts[i++] = c;
-			v_e = list_next(iter_e);
-			v_c = list_next(iter_c);
-		} else if (e < c) {
-			cuts[i++] = e;
-			v_e = list_next(iter_e);
-		} else {
-			cuts[i++] = c;
-			v_c = list_next(iter_c);
-		}
-	}
-
-	while(v_e != NULL) {
-		cuts[i++] = (uint32_t)v_e;
-		v_e = list_next(iter_e);
-	}
-
-	while(v_c != NULL) {
-		cuts[i++] = (uint32_t)v_c;
-		v_c = list_next(iter_c);
-	}
-	// TODO Don't forget to free the iterators
+	int cutCount = mergeListsToArray(list_cuts_edges, list_cuts_colors, cuts);
 
 	list_destroy(list_cuts_edges);
 	list_destroy(list_cuts_colors);
@@ -459,15 +406,68 @@ int processVideo(char *filename, uint32_t *cuts) {
 	av_free(pFrame);
 	sws_freeContext(convert_rgb24);
 	sws_freeContext(convert_g8);
+	return cutCount;
+}
 
-	avcodec_close(pCodecCtx);
-	avformat_close_input(&pFormatCtx);
-	//printf("RESULTS:\n");
-	//for (int i = 0; i < c_cuts; i++) printf("%d\n", cuts[i]);
-	// Better do an extra catch here in case there was a 0 on the list for some reason
-	if (c_cuts != i) {
+int processVideo(char *filename, uint32_t *cuts) {
+	// Struct holding information about container-file
+	AVFormatContext *pFormatCtx = NULL;
+
+	// Read format information into struct
+	if(avformat_open_input(&pFormatCtx, filename, NULL, NULL) != 0) {
 		// TODO Errorhandleing
 		return -1;
 	}
-	return c_cuts;
+	
+	// Retrieve stream information
+	if(avformat_find_stream_info(pFormatCtx, NULL) < 0) {
+		// TODO Errorhandleing
+		return -1; // Couldn't find stream information
+	}
+	
+	// Search for a video stream, assuming there's only one, we pick the first and continue
+	int videoStream = -1;
+	for (int i = 0; i < pFormatCtx->nb_streams; i++ ) {
+		if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
+			videoStream = i;
+			break;
+		}
+	}
+	
+	// Could not find a video stream
+	if (videoStream == -1) {
+		// TODO Errorhandleing
+		return -1;
+	}
+
+	// Retrieve CodecContext
+	AVCodecContext *pCodecCtx = pFormatCtx->streams[videoStream]->codec;
+	
+	// Get the appropiate Decoder for the video stream
+	AVCodec *pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
+	if (pCodec == NULL) {
+		// TODO Errorhandleing
+		printf("Unsupported Codec\n");
+		return -1;
+	}
+	
+	if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0) {
+		// TODO Errorhandleing
+		return -1;
+	}
+
+	// >>>>> DO PROCESSING HERE <<<<<
+	uint32_t cutCount = findCuts(pFormatCtx, pCodecCtx, videoStream, cuts);
+
+	avcodec_close(pCodecCtx);
+	avformat_close_input(&pFormatCtx);
+
+	if (cutCount < 0) {
+		// TODO Errorhandleing / frees
+		return -1;
+	}
+	//printf("RESULTS:\n");
+	//for (int i = 0; i < c_cuts; i++) printf("%d\n", cuts[i]);
+	// Better do an extra catch here in case there was a 0 on the list for some reason
+	return cutCount;
 }
