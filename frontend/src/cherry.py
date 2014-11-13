e56b7e4966a81ba07929c85e4eaa7b22488716f5
Patrick Henrici <patrick@opeey.de>
2014-11-13 19:22:49 +0100

    added search and uploading

--- frontend/src/cherry.py
+++ frontend/src/cherry.py
@@ -1,5 +1,6 @@
 import cherrypy
 import pymongo
+import shutil
 import os
 import re
 
@@ -7,17 +8,78 @@ MONGOCLIENT = pymongo.MongoClient()
 DB = MONGOCLIENT['findvid']
 VIDEOS = DB['videos']
 
-HTMLDIR = os.path.join(os.path.abspath('.'), "html")
+HTMLDIR = os.path.join(os.path.abspath('.'), 'html')
 DEBUG = True
+
+UPLOADDIR = os.path.join('./', 'uploads')
 
 def renderTemplate(filename, config):
 	tplfile = open(os.path.join(HTMLDIR, filename)).read()
 
 	# Replace each placeholder with the information in config
 	for key, value in config.items():
-		tplfile = re.sub(re.escape("<!--###"+key.upper()+"###-->"), str(value), tplfile)
+		tplfile = re.sub(re.escape('<!--###'+key.upper()+'###-->'), str(value), tplfile)
 
 	return tplfile
+
+def formatTime(frame, fps):
+	lengthInSec = int(frame/fps)
+	seconds = lengthInSec % 60
+	minutes = int(lengthInSec / 60) % 60
+	hours = int(lengthInSec / 60 / 60) % 60
+
+	return '%1.2d' % hours + ':' + '%1.2d' % minutes + ':' + '%1.2d' % seconds
+
+def getUploads():
+
+	# todo: only find videos where upload=true
+	uploadsFromDb = VIDEOS.find()
+
+	uploads = []
+
+	for upload in uploadsFromDb:
+		progress = "100%"
+
+		fps = int(upload['fps'])
+		filename = str(upload['filename'])
+		scenecount = len(upload['scenes'])
+		vidid = str(upload['_id'])
+
+		uploadconfig = {
+			'progress': progress,
+			'thumbnail': 'images/thumb.png',
+			'videoid': vidid,
+			'scenecount': scenecount,
+			'filename': filename,
+			'length': formatTime(int(upload['framecount']), fps)
+		}
+		
+		uploads.append(renderTemplate('upload.html', uploadconfig))
+
+	uploadsContent = ""
+	for upload in uploads:
+		uploadsContent += upload
+
+	return uploadsContent
+
+#def upload():
+	#filename = os.path.basename(uploadfile.filename)
+	#extension = os.path.splitext(filename)[1][1:]
+#
+#	#savedFile = os.path.join(UPLOADDIR, filename)
+#
+#	#if os.path.exists(savedFile):
+#	#	return False
+#
+#	#fileOut = open(savedFile, "w")
+#	#fileIn = uploadfile.file.read()
+#	#fileOut.write(fileIn)
+#	#fileOut.close()
+#
+	# call the python server
+
+	#return True
+
 
 class Root(object):
 	@cherrypy.expose
@@ -29,67 +91,137 @@ class Root(object):
 		for video in videosFromDb:
 			fps = int(video['fps'])
 			filename = str(video['filename'])
-
-			lengthInSec = int(int(video['framecount'])/fps)
-			lengthSec = lengthInSec % 60
-			lengthMin = int(lengthInSec / 60) % 60
-			lengthHr = int(lengthInSec / 60 / 60) % 60
+			vidid = str(video['_id'])
 
 			videoconfig = {
 				'thumbnail': 'images/thumb.png',
+				'videoid': vidid,
 				'filename': filename,
-				'length': "%1.2d" % lengthHr + ":" + "%1.2d" % lengthMin + ":" + "%1.2d" % lengthSec
+				'length': formatTime(int(video['framecount']), fps)
 			}
-
-			scenes = []
-
-			for scene in video['scenes']:
-
-				startInSec = int(int(scene['startframe'])/fps)
-				startSec = startInSec % 60
-				startMin = int(startInSec / 60) % 60
-				startHr = int(startInSec / 60 / 60) % 60
- 
-				endInSec = int(int(scene['endframe'])/fps)
-				endSec = endInSec % 60
-				endMin = int(endInSec / 60) % 60
-				endHr = int(endInSec / 60 / 60) % 60
-
-				sceneconfig = {
-					'thumbnail': 'images/thumb.png',
-					'filename': filename,
-					'scenecount': str(int(scene['_id'])),
-					'starttime': "%1.2d" % startHr + ":" + "%1.2d" % startMin + ":" + "%1.2d" % startSec,
-					'endtime': "%1.2d" % endHr + ":" + "%1.2d" % endMin + ":" + "%1.2d" % endSec
-				}
-
-				scenes.append(renderTemplate('scene.html', sceneconfig))
-
-			videos.append({'html': renderTemplate('video.html', videoconfig), 'scenes': scenes})
+			
+			videos.append(renderTemplate('video.html', videoconfig))
 
 		content = ""
 		for video in videos:
-			for scene in video['scenes']:
-				content += scene
+			content += video
 
 		config = {
-			'title': 'find.vid - The Videosearch Engine',
+			'title': 'find.vid - The Videosearch Engine: Videos',
 			'videocount': '0',
 			'scenecount': '0',
+			'searchterm': '',
+			'uploads': getUploads(),
 			'content': content
 		}
 		return renderTemplate('template.html', config)
 
-class Test(object):
 	@cherrypy.expose
-	def index(self):
-		return "test"
+	def upload(self):
+		filename = os.path.basename(cherrypy.request.headers['x-filename'])
+		destination = os.path.join(UPLOADDIR, filename)
+		with open(destination, 'wb') as f:
+			shutil.copyfileobj(cherrypy.request.body, f)
+
+class Video(object):
+	@cherrypy.expose
+	def index(self, vidid = None):
+		if not vidid:
+			raise cherrypy.HTTPRedirect('/')
+
+		videoFromDb = VIDEOS.find_one({'_id': str(vidid)})
+
+		if not videoFromDb:
+			raise cherrypy.HTTPRedirect('/')
+
+		scenes = []
+		path = videoFromDb['path']
+		filename = videoFromDb['filename']
+		fullpath = os.path.join(path, filename)
+		fps = videoFromDb['fps']
+
+		for scene in videoFromDb['scenes']:
+
+			sceneconfig = {
+				'url': fullpath,
+				'extension': os.path.splitext(filename)[1][1:],
+				'time': str(scene['startframe'] / fps),
+				'thumbnail': 'images/thumb.png',
+				'filename': filename,
+				'scenecount': str(int(scene['_id'])),
+				'starttime': formatTime(int(scene['startframe']), fps),
+				'endtime': formatTime(int(scene['endframe']), fps)
+			}
+
+			scenes.append(renderTemplate('scene.html', sceneconfig))
+
+		content = ""
+		for scene in scenes:
+			content += scene
+
+		config = {
+			'title': 'find.vid - The Videosearch Engine: Scenes',
+			'videocount': '0',
+			'scenecount': '0',
+			'searchterm': '',
+			'uploads': getUploads(),
+			'content': content
+		}
+
+		return renderTemplate('template.html', config)
+
+
+class Search(object):
+	@cherrypy.expose
+	def index(self, name = None):
+		if not name:
+			raise cherrypy.HTTPRedirect('/')
+
+		videosFromDb = VIDEOS.find({"filename": { '$regex': name} })
+
+		if not videosFromDb:
+			content = 'No Videos found.'
+		else:
+			videos = []
+
+			for video in videosFromDb:
+				fps = int(video['fps'])
+				filename = str(video['filename'])
+				vidid = str(video['_id'])
+
+				videoconfig = {
+					'thumbnail': 'images/thumb.png',
+					'videoid': vidid,
+					'filename': filename,
+					'length': formatTime(int(video['framecount']), fps)
+				}
+				
+				videos.append(renderTemplate('video.html', videoconfig))
+
+			content = ""
+			for video in videos:
+				content += video
+
+		config = {
+			'title': 'find.vid - The Videosearch Engine: Search',
+			'videocount': '0',
+			'scenecount': '0',
+			'searchterm': name,
+			'uploads': getUploads(),
+			'content': content
+		}
+
+		return renderTemplate('template.html', config)
+
 
 if __name__ == '__main__':
 	cherrypy.config.update('./global.conf')
 	
-	cherrypy.tree.mount(Root(), "/", './app.conf')
-	cherrypy.tree.mount(Test(), "/test", './app.conf')
+	cherrypy.tree.mount(Root(), '/', './app.conf')
+	cherrypy.tree.mount(Video(), '/video', './app.conf')
+	cherrypy.tree.mount(Search(), '/search', './app.conf')
+
+	cherrypy.server.max_request_body_size = 0
 
 	if hasattr(cherrypy.engine, 'block'):
 		# 3.1 syntax
--- frontend/src/js/interface.js
+++ frontend/src/js/interface.js
@@ -1,13 +1,128 @@
 $(function() {
+	function upload(file) {
+		var xhr = new XMLHttpRequest();
+
+		$('.uploadprogress').show();
+
+		xhr.upload.addEventListener('progress', function(event) {
+			console.log('progess', file.name, event.loaded, event.total);
+
+			var perc = Math.round((event.loaded / event.total) * 100);
+
+			if (event.loaded == event.total) {
+				$('.uploadprogress').hide();
+			}
+
+			$('.uploadprogress .label.progress').html(perc + '%');
+
+		});
+		xhr.addEventListener('readystatechange', function(event) {
+			console.log(
+				'ready state', 
+				file.name, 
+				xhr.readyState, 
+				xhr.readyState == 4 && xhr.status
+		 	);
+		});
+
+		xhr.open('POST', '/upload', true);
+		xhr.setRequestHeader('X-Filename', file.name);
+
+		console.log('sending', file.name, file);
+		xhr.send(file);
+	}
+
 	$('.menu .searchnavi .icon.closeicon').on('click', function() {
 		$('.menu .searchnavi .searchfield').val('');
 	});
 
 	$('.uploadwindow').on('mouseenter', function() {
-		$('.uploadwrap').addClass('active');
+		$('.upload-wrap').addClass('active');
 	});
 
 	$('.uploadwindow').on('mouseleave', function() {
-		$('.uploadwrap').removeClass('active');
+		$('.upload-wrap').removeClass('active');
 	});
+
+	$('.button.search').on('click', function() {
+		$.ajax({
+			url: '/search/',
+			data: {'name':$('.searchbar .searchfield').val()},
+			type: 'GET',
+			
+			beforeSend: function() {
+				$('#content').html('Loading...');
+			},
+
+			success: function(data) {
+				$('#content').html($(data).find('#content').html());
+			}
+		});
+
+		//window.location.href =  + $('.searchbar .searchfield').val();
+	});
+
+	$('.button.upload').on('click', function() {
+		$('#uploadfile').click();
+	});
+
+	$('#uploadfile').change(function(event) {
+		for(var i = 0; i < event.target.files.length; i += 1) {
+			upload(event.target.files[i]); 
+		}
+		//$('.uploadform').reset();
+	});
+
+	$('.scene').on('click', function() {
+		var $this = $(this),
+
+			videourl = $this.data('url'),
+			format = $this.data('extension'),
+			time = $this.data('time'),
+
+			title = $this.find('.meta .label.title').html() + " " + $this.find('.meta .label.scenecount').html(),
+			poster = $this.find('.thumbnail img').attr('src'),
+
+			$videoplayerWrap = $('.videoplayer-wrap'),
+			$overlayLabel = $videoplayerWrap.find('.overlay .label.title'),
+			$videoplayer = $videoplayerWrap.find('video');
+
+		$overlayLabel.html(title);
+		$videoplayer.attr('poster', poster);
+
+		$videoplayer.find('source').attr('src', videourl).attr('type', 'video/' + format);
+
+		$videoplayer.load();
+
+		$videoplayer.ready(function() {
+
+			$videoplayerWrap.fadeIn(500);
+
+			$videoplayerWrap.promise().done(function() {
+				var video = $videoplayer[0];
+
+				video.currentTime = parseFloat(time);
+				video.play();
+
+				$videoplayer.on('play', function (e) {
+					$('.videoplayer-wrap .videoplayer .overlay .button.searchscene').hide();
+				});
+
+				$videoplayer.on('pause', function (e) {
+					$('.videoplayer-wrap .videoplayer .overlay .button.searchscene').show();
+				});
+
+			});
+
+		});
+	});
+
+	$('.videoplayer-wrap .videoplayer .overlay .button.close').on('click', function() {
+		var $videoplayerWrap = $('.videoplayer-wrap');
+
+		$videoplayerWrap.find('video')[0].pause();
+
+		$videoplayerWrap.hide();
+	});
+
 });
