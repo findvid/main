997e7f5d0ab198b129caf1532c21d2887166b820
Dennis Müller <dennis.mueller@student.hs-rm.de>
2014-11-17 09:33:05 +0100

    Canny Edge Detector

--- impl/src/edgedetect.c
+++ impl/src/edgedetect.c
@@ -1,6 +1,9 @@
 #include <math.h>
 #include "edgedetect.h"
 
+#define HYSTERESIS_T1 16
+#define HYSTERESIS_T2 4
+
 //Improve contrast by linear scaling
 void linearScale(AVFrame * pic, int width, int height) {
 	int min = 255, max = 0;
@@ -124,18 +127,25 @@ void getSobelOutput(AVFrame * gray, struct t_sobelOutput * out, int width, int h
 			mag->data[0][x + y * mag->linesize[0]] = (uint8_t)magvar;
 
 			double dirvar = atan2(dx, dy);
+			
 			//Encode direction into a gray value: [-90|90] -> [0|3]
 			//-180 = 0
 			if (dirvar < 0) //]-90|0[ -> [180|90[
-				dirvar = (M_PI / 2) - dirvar;
+				dirvar = M_PI + dirvar;
 			//[0|180] -> [0|255]
 			//No need for drastic precision, use multiples of 45 degrees
-			dirvar = round(dirvar / (M_PI /4));
-			printf("dirvarf = %f\n", dirvar);
+			if (dirvar < 0 || dirvar > (M_PI))
+				printf("invalid dirvar = %f\n", dirvar);
+
+			dirvar = round(dirvar / (M_PI / 4));
+			if (dirvar > 4.0)
+				printf("invalid dircode = %f\n", dirvar);
 			//dirvar *= (M_PI / 4);
-			//dirvar = (dirvar/(2 * M_PI)) * 255;
+			//dirvar = (dirvar/(M_PI)) * 255;
+
 
 			dir->data[0][x + y * dir->linesize[0]] = (uint8_t)dirvar;
+			//printf("dirvarf = %d\n", getPixelG8(dir, x, y));
 		}
 	}
 
@@ -180,27 +190,31 @@ AVFrame * getEdgeProfile(AVFrame * original, struct SwsContext * ctx, int width,
 					ox = 1;
 					oy = 0;
 					break;
-				case 3:
+				case 3 //135 degree:
 					ox = 1;
 					oy = 1;
 					break;
+				default:
+					printf("INVALID DIRCODE AT (%d, %d)\n", x, y);
 			}
 			if ((getPixelG8(sobel.mag, x, y) < getPixelG8(sobel.mag, x-oy, y-oy)) || (getPixelG8(sobel.mag, x,y) < getPixelG8(sobel.mag, x+ox, y+oy)))
 				setPixelG8(sobel.mag, x, y, 0);
 		}
 	}
 
+	linearScale(sobel.mag, width, height);
 	SaveFrameG8(sobel.mag, width, height, 5);
 
 	//Step 4:Hysteresis thresholding
 	AVFrame * res = av_frame_alloc();
 	avpicture_alloc((AVPicture *)res, PIX_FMT_GRAY8, width, height);
 
-	
+
 	for (int x = 0; x < width; x++) {
 		for (int y = 0; y < width; y++) {
-			if (getPixelG8(sobel.mag, x, y) > 63) {
+			if (getPixelG8(sobel.mag, x, y) > HYSTERESIS_T1) {
 				setPixelG8(res, x, y, 255);
+				setPixelG8(sobel.mag, x, y, 0);
 				int ox = 0, oy = 0;
 				switch(getPixelG8(sobel.dir, x, y))  {
 					case 0: //0 degree
@@ -223,7 +237,7 @@ AVFrame * getEdgeProfile(AVFrame * original, struct SwsContext * ctx, int width,
 				}
 				//Follow edge in "+"-direction
 				int ox2 = ox, oy2 = oy;
-				while (getPixelG8(sobel.mag, x+ox2, y+oy2) > 48) {
+				while (getPixelG8(sobel.mag, x+ox2, y+oy2) > HYSTERESIS_T2) {
 //					printf("Follow(+) edge to (%d,%d)\n", (x+ox2), (y+oy2));
 					//Set this as edge pixel
 					setPixelG8(res, x+ox2, y+oy2, 255);
@@ -233,13 +247,14 @@ AVFrame * getEdgeProfile(AVFrame * original, struct SwsContext * ctx, int width,
 					oy2 += oy;
 				}
 				//Repeat in "-"-direction
-				ox2 = ox, oy2 = oy;
-				while (getPixelG8(sobel.mag, x-ox2, y-oy2) > 48) {
+				ox2 = ox;
+				oy2 = oy;
+				while (getPixelG8(sobel.mag, x-ox2, y-oy2) > HYSTERESIS_T2) {
 //					printf("Follow(-) edge to (%d,%d)\n", (x-ox2), (y-oy2));
 					//Set this as edge pixel
-					setPixelG8(res, x+ox2, y+oy2, 255);
+					setPixelG8(res, x-ox2, y-oy2, 255);
 					//No need to repeat this
-					setPixelG8(sobel.mag, x+ox2, y+oy2, 0);
+					setPixelG8(sobel.mag, x-ox2, y-oy2, 0);
 					ox2 += ox;
 					oy2 += oy;
 				}
