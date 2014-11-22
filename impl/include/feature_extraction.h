267d4657f70c038008dbf752ca366450e8b16fca
Dennis Müller <dennis.mueller@student.hs-rm.de>
2014-11-22 11:01:05 +0100

    Skeleton for feature extraction without largelists

---
+++ impl/include/feature_extraction.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "fvutils.h"
+#include "largelist.h"
+
+#define FEATURE_AMNT 4
+
+typedef struct {
+	uint32_t ** feature_list;
+	uint32_t * feature_length;
+	uint32_t feature_count;
+} FeatureTuple;
+
+FeatureTuple * getFeatures(char *, char*, int, uint32_t *, int);
