bfa132ca2c41fc325a83569e20bbdf9f9e141c56
Dennis Müller <dennis.mueller@student.hs-rm.de>
2014-11-25 10:54:13 +0100

    Robustness against unextractable features(keyframe(s) outside of the video)

--- impl/src/feature_extraction.c
+++ impl/src/feature_extraction.c
@@ -162,6 +162,14 @@ FeatureTuple * getFeatures(char * filename, char * expath, int vidThumb, uint32_
 		currentFrame++;
 		readFrame(iter, frame, &gotFrame);
 	}
+	if (currentScene < sceneCount) {
+		int diff = (sceneCount - currentScene);
+		for (int i = (res->feature_count - 1); i >= (res->feature_count - diff); i--) {
+			free(res->feature_list[i]);
+			printf("Freeing feature_list[%d]\n", i);
+		}
+		res->feature_count -= diff;
+	}
 	av_frame_free(&frame);
 	av_free(buffer);
 	free(videoName);
