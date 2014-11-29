fae5ffa083f71b2f109898a58f9128da690ed58e
Johannes <adnap9449@gmail.com>
2014-11-29 19:53:43 +0100

    Added some documentation and #pragma once

--- impl/include/shotbounds.h
+++ impl/include/shotbounds.h
@@ -1,1 +1,13 @@
+#pragma once
+
+/**
+ * Detects cuts in a video.
+ *
+ * @param filename	Path to the video file
+ * @param cuts		Gets filled with a newly allocated
+ *			array containing the detected cuts
+ *
+ * @return		On success the amount of detected cuts
+ *			On failure a negative error value
+ */
 int processVideo(const char *filename, uint32_t **cuts);
