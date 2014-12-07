0a3a99123199fb4972ec4349e2e288b48e04251c
Patrick Henrici <patrick.henrici@student.hs-rm.de>
2014-12-07 13:54:10 +0100

    added (wrong) fourier transformation

--- impl/gist.py
+++ impl/gist.py
@@ -6,23 +6,45 @@ import cv2
 import argparse
 import os
 import sys
+import math
 
-IMGWIDTH = 640
-IMGHEIGHT = 480
+IMGWIDTH = 32
+IMGHEIGHT = 32
 
 RASTERSIZE = 4
 
 def errorMessage(msg):
-	print '\033[91m' + msg + '\033[92m'
+	print '\033[91m' + msg + '\033[0m'
 
 def infoMessage(msg):
 	print msg
 
 def successMessage(msg):
-	print '\033[92m' + msg + '\033[92m'
+	print '\033[92m' + msg + '\033[0m'
 
 def warningMessage(msg):
-	print '\033[93m' + msg + '\033[92m'
+	print '\033[93m' + msg + '\033[0m'
+
+def fourier(image, N):
+	# F(x,y) = N-1 sum(i=0)( N-1 sum(j=0) (f(i,j)*e^-i*2*pi*( (x*i/N) + (y*j/N) )))
+	# N = image of size NxN
+	# f(i,j) = image in the spatial domain 
+	# exp-term = basis function corresponding to each Point F(k,l) in the Fourier space
+	# let's go...
+
+	fourier = np.zeros((N, N))
+
+	# For each pixel in the NxN image
+	for x in range(0, N-1):
+		for y in range(0, N-1):
+			# Calculate fourier value
+			for i in range(0, N-1):
+				for j in range(0, N-1):
+					fourier[x][y] += image[i][j] * math.exp( ((x*i)/N) + ((y*j)/N) )
+
+	return fourier
+
+
 
 if __name__ == "__main__":
 
@@ -41,7 +63,7 @@ if __name__ == "__main__":
 	filename = splitext[0]
 	extension = splitext[1]
 
-	image = cv2.imread(imgPath)
+	image = cv2.imread(imgPath, cv2.CV_LOAD_IMAGE_GRAYSCALE)
 
 	if (image is None) or (len(image) == 0):
 		errorMessage("The file '" + imgPath + "' couldn't be read. (Not an image?)")
@@ -65,7 +87,7 @@ if __name__ == "__main__":
 	xRaster = IMGHEIGHT / RASTERSIZE
 
 	# create zero-filled raster
-	imgRaster = np.zeros((RASTERSIZE, RASTERSIZE, xRaster, yRaster, 3))
+	imgRaster = np.zeros((RASTERSIZE, RASTERSIZE, xRaster, yRaster))
 
 	x = 0
 	for line in imgRaster:
@@ -74,3 +96,7 @@ if __name__ == "__main__":
 			imgRaster[x][y] = np.copy(imgResized[(x+0)*xRaster:(x+1)*xRaster, (y+0)*yRaster:(y+1)*yRaster])
 			y+=1
 		x+=1
+
+	cv2.imwrite(filename + '_fourier.' + extension, fourier(imgResized, 32))
+
+	##### Orientation Histogram #####
