0912bf7a285e910cb4d3238dd7bc064c46a01493
Dennis Müller <dennis.mueller@student.hs-rm.de>
2015-01-13 16:42:56 +0100

    Commit für checkout

--- impl/test.py
+++ impl/test.py
@@ -1,5 +1,5 @@
 import FindVid as fv
 
-print (fv.getFeatures("testfiles/hardcuts.mp4"))
+print (fv.getFeatures("testfiles/hardcuts.mp4", "0xFUCKU", 50, [50], "/home/kanonenfutter/tmp"))
 
 print (fv.getFramerate("testfiles/hardcuts.mp4"))
