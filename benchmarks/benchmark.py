55847c1b2e6d5ab1e38e764a9879c9c5dadd5111
Patrick Henrici <patrick.henrici@student.hs-rm.de>
2015-01-18 16:55:28 +0100

    plotting fancy graphes

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -6,6 +6,11 @@ import re
 import urllib2
 import subprocess
 import time
+
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
 import kmeanstree
 
 from sys import exit
@@ -21,11 +26,9 @@ PARSER.add_argument('database', metavar='DB',
 PARSER.add_argument('collection', metavar='COLLECTION',
 	help='The name of the Collection in the Database')
 PARSER.add_argument('origpath', metavar='ORIGINALPATH',
-	help='The original path, where the queries and targets are (absolute!!!). This is used for the informations in the output files.')
-PARSER.add_argument('out', metavar='OUT',
-	help='The output path (Each benchmark creates a folder and its files in it).')
+	help='The original path, where the queries and targets are (absolute!!!). This is used for the informations in the output files and reading the groundtruth file.')
 PARSER.add_argument('tolerance', metavar='TOLERANCE',
-	help='The tolerance (in %) how many frames the found target can be away from ground truth')
+	help='The tolerance (in frames) how many frames the found target can be away from ground truth')
 
 # parse input arguments
 ARGS = PARSER.parse_args()
@@ -35,15 +38,13 @@ PORT = ARGS.port
 DBNAME = ARGS.database
 COLNAME = ARGS.collection
 ORIGPATH = ARGS.origpath
-OUTPATH = ARGS.out
-TOLERANCE = ARGS.tolerance
+TOLERANCE = int(ARGS.tolerance)
+
+# Directory of this file
+ROOTDIR = os.path.abspath('.')
 
 if (not os.path.exists(SERVERFILE)) or (not os.path.isfile(SERVERFILE)):
 	print "The webserver file: '" + SERVERFILE + "', doesn't exist or is not a file!"
-	sys.exit(1)
-
-if (not os.path.exists(OUTPATH)) or (not os.path.isdir(OUTPATH)):
-	print "The given path: '" + OUTPATH + "', doesn't exist or is not a directory!"
 	sys.exit(1)
 
 if (not os.path.exists(ORIGPATH)) or (not os.path.isdir(ORIGPATH)):
@@ -75,15 +76,15 @@ COLORNORMAL = '\033[0m'
 COLORWARNING = '\033[93m'
 COLORFAIL = '\033[91m'
 
-def startWebserver(ksplit, kmax):
-	command = 'python ' + SERVERFILE + ' ' + PORT + ' ' + DBNAME + ' ' + COLNAME + ' ' + str(ksplit) + ' ' + str(kmax) + ' searchtree_benchmark.db --quiet'
+def startWebserver(ksplit, imax, dbfile):
+	command = 'python ' + SERVERFILE + ' ' + PORT + ' ' + DBNAME + ' ' + COLNAME + ' ' + str(ksplit) + ' ' + str(imax) + ' ' + str(dbfile) + ' --quiet'
 	return subprocess.Popen('exec ' + command, stdout=subprocess.PIPE, shell=True)
 
 def stopWebserver(process):
 	process.terminate()
 	return process.wait()
 
-def benchmarkTreeBuild():
+def benchmarkTreeBuild(outdir='./out'):
 	videos = VIDEOS.find({'_id': {'$not': {'$eq': 'config'}}}, {'scenes': 0})
 
 	videocount = videos.count()
@@ -94,46 +95,98 @@ def benchmarkTreeBuild():
 
 	print "Building the trees with " + str(videocount) + " Videos and " + str(scenecount) + " Scenes\n"
 	
-	for power in range(0, 8):	
-		ksplit = 2**power
-		kmax = 2**power
-		print "Bulding tree - ksplit: " + str(ksplit) + "  -  kmax: " + str(kmax)
+	for i in range(0, 2):
 
-		starttime = time.time()
-		kmeanstree.loadOrBuildAndSaveTree(videos=VIDEOS, filename='./temptree.db', k=ksplit, imax=kmax)
-		endtime = time.time()
+		paramValues = []
+		timeValues = []
+		hitValues = []
+		searchTimeValues = []
+		for power in range(0, 8):
+			if i == 0:
+				ksplit = 2**power
+				imax = 100
+			else:
+				ksplit = 8
+				imax = 2**power
 
-		os.remove('./temptree.db')
+			paramValues.append(power)
 
-		difftime = endtime-starttime
-		print "Builded tree in " + str(difftime) + " seconds\n"
-		print "Testing SceneSearch"
+			print "Bulding tree - ksplit: " + str(ksplit) + "  -  imax: " + str(imax)
 
+			starttime = time.time()
+			kmeanstree.loadOrBuildAndSaveTree(videos=VIDEOS, filename=os.path.join(ROOTDIR, 'temptree.db'), k=ksplit, imax=imax)
+			endtime = time.time()
 
+			difftime = endtime-starttime
+			print "Builded tree in " + str(difftime) + " seconds"
+
+			timeValues.append(difftime)
+
+			print "Testing SceneSearch..."
+			process = startWebserver(ksplit=ksplit, imax=imax, dbfile=os.path.join(ROOTDIR, 'temptree.db'))
+			searchBenchmark = sceneSearch(limit=10)
+			stopWebserver(process)
+
+			hitValues.append(searchBenchmark['correct'])
+			searchTimeValues.append(searchBenchmark['averagetime'])
+
+			print str(searchBenchmark['correct']) + "% hitrate"
+			print "In average " + str(searchBenchmark['averagetime']) + " seconds per searchquery\n"
+
+			os.remove(os.path.join(ROOTDIR, 'temptree.db'))
+
+		if i == 0:
+			label = "ksplit"
+		else:
+			label = "imax"
+
+		plt.subplot(311)
+		plt.title('Time of tree builds')
+		plt.plot(paramValues, timeValues, 'ro')
+		plt.xlabel(label)
+		plt.ylabel('time in seconds')
+
+		plt.subplot(312)
+		plt.title('Hitrate of trees')
+		plt.plot(paramValues, hitValues, 'ro')
+		plt.xlabel(label)
+		plt.ylabel('hits in %')
+
+		plt.subplot(313)
+		plt.title('Average time per scenesearch of trees')
+		plt.plot(paramValues, searchTimeValues, 'ro')
+		plt.xlabel(label)
+		plt.ylabel('avrg. time in seconds')
+
+		plt.tight_layout()
+		plt.savefig(os.path.join(outdir, label + ".png"))
+
+		plt.gcf().clear()
+
+def benchmarkSceneSearch(outdir='./out'):
+	#for i in range(0, 10):
 	return True
+
 
 # Search for all videos starting with "query" in database
 # RegEx: Search for substring "query" in path, with digits after the string and a period after the digits
 # (so we can be sure 'query' is not some directory name or else...)
-def benchmarkSceneSearch(limit=100, nnlimit=1000):
+def sceneSearch(limit=100, nnlimit=1000, outdir='./out'):
 	queryvideos = VIDEOS.find({'filename': {'$regex': 'query\d*\.'}}, {'filename': 1, 'cuts': 1})
 
 	count = queryvideos.count()
 
-	#print str(count) + " videos starting with 'query' where found in database.\n"
-	#outfile = open(os.path.join(path, 'out.txt'), 'w')
-
+	totalTime = 0
+	correct = 0
 	for video in queryvideos:
 
 		vidid = video['_id']
 		filename = video['filename']
 		scenecount = len(video['cuts']) - 1
-		
-		#print "Processing video: '"+str(filename)+"' - '"+str(vidid)+"'"
 
 		frame = 0
+		# When the video has more than 1 scene, take the longest
 		if scenecount > 1:
-			#print COLORWARNING + "WARNING! - The query-video has more than 1 scene. Using the longer one!" + COLORNORMAL
 			length = 0
 			for cut in range(0, scenecount):
 				thislength = video['cuts'][cut+1]-1 - video['cuts'][cut]
@@ -143,43 +196,90 @@ def benchmarkSceneSearch(limit=100, nnlimit=1000):
 
 		response = False
 		tries = 0
-		while not response and tries < 3:
+		while not response:
 			try:
 				starttime = time.time()
-				response = urllib2.urlopen('http://localhost:'+str(PORT)+'/searchSceneList?vidid='+str(vidid)+'&frame='+str(frame)+'&limit='+limit+'&nnlimit='+nnlimit)
+				response = urllib2.urlopen('http://localhost:'+str(PORT)+'/searchSceneList?vidid='+str(vidid)+'&frame='+str(frame)+'&limit='+str(limit)+'&nnlimit='+str(nnlimit))
 				body = response.read()
 				endtime = time.time()
 			except urllib2.URLError:
-				#print COLORFAIL + "Webserver not reachable! Trying again in 10 seconds..." + COLORNORMAL
+				# Webserver is not ready - wait 10 seconds and try again
 				time.sleep(10)
 				response = False
-			tries+=1
-		
-		if tries >= 3:
-			print "\n" + COLORFAIL + "ERROR! - Webserver could not be reached after 3 retries." + COLORNORMAL
-			sys.exit(1)
+
+		results = body.split('\n')
+		for lineNum in range(0, len(results)):
+			results[lineNum] = results[lineNum].split()
+
+		gtline = None
+		basename = os.path.splitext(os.path.basename(filename))[0]
+		for query in GTDATA:
+			# The last splitted entry can be empty
+			if len(query) == 0:
+				continue
+
+			gtfilename = os.path.splitext(os.path.basename(query[0]))[0]
+			if basename == gtfilename:
+				gtline = query
+				break
+
+		if not gtline:
+			print COLORFAIL + "ERROR: Video not found in Ground Truth! Skipping... ("+basename+")" + COLORNORMAL
+			count-=1
+			continue
 
 		difftime = endtime-starttime
+		totalTime += difftime
+
+		counter = 1.0
+		for result in results:
+			# The last splitted entry can be empty
+			if len(result) == 0:
+				continue
+
+			# Same target file
+			if os.path.splitext(os.path.basename(result[0]))[0] == os.path.splitext(os.path.basename(gtline[1]))[0]:
+				# Check if time values are within tolerance
+
+				startResult = int(result[1])
+				endResult = int(result[2])
+
+				startGt = int(gtline[2])
+				endGt = int(gtline[3])
+
+				# If the start value is not in tolerance range of groundtruth value: continue
+				if not ((startResult >= startGt-TOLERANCE) and (startResult <= startGt+TOLERANCE)):
+					continue
+
+				# If the end value is not in tolerance range of groundtruth value: continue
+				if not ((endResult >= endGt-TOLERANCE) and (endResult <= endGt+TOLERANCE)):
+					continue
+
+				# If we got here, congratulation. We found a correct target.
+				correct += counter
+				break
+
+			counter-=0.1
+	
 
 		#outfile.write(str(filename) + ' ' + str(difftime) + '\n')
 		#outfile.write(body)
 
 		#print "Searchquery took " + str(difftime) + " seconds\n"
 
-	outfile.close()
+	correctPerc = correct / float(count) * 100.0
+	averagetime = totalTime / float(count)
+
+	return {'correct': correctPerc, 'averagetime': averagetime}
+
+	#outfile.close()
 	
 if __name__ == '__main__':
-	
-	#process = startWebserver(8, 100)
- 
-	testdir = os.path.join(OUTPATH, 'scenesearch')
+	outdir = os.path.join(ROOTDIR, 'out')
 	try:
-		os.mkdir(testdir)
+		os.mkdir(outdir)
 	except OSError:
-		print COLORWARNING + "WARNING! - The output directory already contains files. Older benchmarks may be overwritten." + COLORNORMAL
-	#benchmarkSceneSearch(testdir)
+		print COLORWARNING + "WARNING: Output directory already exists. Existing data may be overwritten." + COLORNORMAL
 
-	#stopWebserver(process)
-	
-	print "Testing Tree Times..."
-	benchmarkTreeBuild()
+	benchmarkTreeBuild(outdir)
+
